0aff35b76228ace2f57d62d95b2ee4024cca0b2e
Mickael <mick.rigault@gmail.com>
2015-11-18 20:07:19 +0100

    an instrument function to fetch tem all

---
+++ instruments/instrument.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# -- here load all the object that could be parsed
+from ..utils.decorators import _autogen_docstring_inheritance
+from ..astrobject.photometry import photopoint,Image
+
+from .sdss import sdss,SDSS_INFO,is_sdss_file
+from .hst  import hst,HST_INFO,is_hst_file
+
+__all__ = ["instrument","KNOWN_INSTRUMENTS"]
+
+KNOWN_INSTRUMENTS = ["sdss","hst"]
+
+
+def instrument(filename,astrotarget=None,**kwargs):
+    """This method parse the input file to know to which
+    instrument this filename is associated to.
+    This methods will return the corresponding object"""
+    
+    # - SDSS 
+    if is_sdss_file(filename):
+        return sdss(filename,astrotarget=astrotarget,
+                    **kwargs)
+    # - HST     
+    if is_hst_file(filename):
+        return hst(filename,astrotarget=astrotarget,
+                    **kwargs)
+    
+    # - Nothing else...
+    raise ValueError("'filename' does not belongs to a known instrument "+"\n"+\
+                     "these are:"+", ".join(KNOWN_INSTRUMENTS))
+                     
