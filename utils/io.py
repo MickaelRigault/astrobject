ccda4d1c2fa850200143e6dd6243f5fb426e7529
Mickael <mick.rigault@gmail.com>
2016-01-17 15:36:06 +0100

    Dust Map directory dependency moved to astropy

--- utils/io.py
+++ utils/io.py
@@ -9,9 +9,7 @@ import numpy as np
 import warnings
 from glob import glob
             
-DEFAULT_SF98_DUSTDIR = os.path.dirname(__file__)+"/data/dustmap/"
-DEFAULT_SFD_FILES = ["SFD_dust_4096_ngp.fits","SFD_mask_4096_ngp.fits",
-                     "SFD_dust_4096_sgp.fits","SFD_mask_4096_sgp.fits"]
+DEFAULT_SFD_FILES = ["SFD_dust_4096_ngp.fits","SFD_dust_4096_sgp.fits"]
 
 ############################################
 #                                          #
@@ -21,17 +19,28 @@ DEFAULT_SFD_FILES = ["SFD_dust_4096_ngp.fits","SFD_mask_4096_ngp.fits",
 def get_default_sfd98_dir(download_if_needed=True):
     """
     """
-    if not test_dustdir(DEFAULT_SF98_DUSTDIR):
-        if dowload_if_needed:
-            download_sf98_dust_map()
+    # -------------------------
+    # - Reads astropy/sncosmo
+    a = [l for l in open(os.path.expanduser('~')+"/.astropy/config/sncosmo.cfg").read().splitlines()
+         if l.startswith("sfd98_dir")]
+    if len(a) == 0:
+        raise IOError("You need to define *sfd98_dir* in the sncosmo/astropy config file: see ~/.astropy/config/sncosmo.cfg")
+    
+    sfd98_dir = a[-1].split('=')[-1].replace(" ","")
+
+    # -------------------------
+    # - Do you have the files
+    if not test_dustdir(sfd98_dir):
+        if download_if_needed:
+            download_sf98_dust_map(sfd98_dir)
         else:
             warnings.warn("No dust map downloaded yet. Do so to have access to SFD98 data")
             return None
         
-    return DEFAULT_SF98_DUSTDIR
-        
+    return sfd98_dir
 
-def download_sf98_dust_map(verbose=True):
+
+def download_sf98_dust_map(outdir,verbose=True):
     """
     This downloads the dustmap from sncosmo
     """
@@ -44,27 +53,28 @@ def download_sf98_dust_map(verbose=True):
         print "Dust map downloading starting. 4 files, 167 Mo in total"
 
     try:
-        os.makedirs(DEFAULT_SF98_DUSTDIR)
-        if verbose: print "Dust files saved here: %s"%DEFAULT_SF98_DUSTDIR
+        os.makedirs(outdir)
+        if verbose: print "Dust files saved here: %s"%outdir
     except:
-        if verbose: print "%s path already exists. Dust map downloading their"%DEFAULT_SF98_DUSTDIR
+        if verbose: print "%s path already exists. Dust map downloading their"%outdir
+            
     for f in ["SFD_dust_4096_ngp.fits","SFD_mask_4096_ngp.fits",
               "SFD_dust_4096_sgp.fits","SFD_mask_4096_sgp.fits"]:
-        wget.download("http://sncosmo.github.io/data/dust/"+f,out=DEFAULT_SF98_DUSTDIR)
+        wget.download("http://sncosmo.github.io/data/dust/"+f,out=outdir)
 
     if verbose:
         print "SF98 dust map downloaded."
 
 
-def test_dustdir(dustdir=DEFAULT_SF98_DUSTDIR,
-                 hard_test=True):
+def test_dustdir(dustdir,hard_test=True):
     """
     """
 
-    filesin = glob(dustdir+"*.fits")
+    filesin = glob(dustdir+"/*.fits")
     if len(filesin)==0:
+        print "no data in %s"%dustdir
         return False
-    if hard_test and np.asarray([dustdir+f not in filesin
+    if hard_test and np.asarray([dustdir+"/"+f not in filesin
                                  for f in DEFAULT_SFD_FILES]).any():
         return False
     return True
