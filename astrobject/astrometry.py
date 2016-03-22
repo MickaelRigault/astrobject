Bug about FoV masking when reload_data()

astrobject/astrometry.py
@@ -69,14 +69,101 @@ class _MotherWCS_( object ):
     """ This shoud be merged with WCS once the aslLib dependency is cleaned """
     __nature__ = "WCS"
 
-    def set_offset(self,offset0,offset1):
+    # ---------------- #
+    # - Converstion  - #
+    # ---------------- #
+    def units_to_pixels(self,units_, target=None):
+        """units should be a parsable string or a astropy units"""
+        
+        if type(units_) == str:
+            # - astropy units
+            if units_.lower() in ["pixel","pixels","pxl","pix","pxls","pixs"]:
+                return units.Quantity(1.)
+            elif units_.lower() in ["kpc"]:
+                if target is None:
+                    raise AttributeError("You need a target to convert kpc->arcsec")
+                units_ = target.arcsec_per_kpc * units.arcsec
+            elif units_ in dir(units):
+                units_ = 1*units.Unit(units_)
+            else:
+                TypeError("unparsable units %s: astropy.units or pixels/pxl or kpc could be provided."%units_)
+        # ----------------
+        # - astropy Units
+        if type(units_) is units.core.Unit:
+            units_ = 1*units_
+
+        if type(units_) is units.quantity.Quantity:
+            return units_.to("degree") / self.pix_indeg
+        
+        raise TypeError("unparsable units: astropy.units or pixels/pxl or kpc could be provided.")
+
+    # ---------------- #
+    # - Offset Stuff - #
+    # ---------------- #
+    def set_offset(self,offset0,offset1, width=None, height=None):
+        self._image_width  = width
+        self._image_height = height
+        self._reload_contours = True
         self._image_offset = -np.asarray([offset0,offset1])
 
+
+    @property
+    def image_width(self):
+        return self._image_width if "_image_width" in dir(self) and self._image_width is not None\
+          else self._naxis2
+          
+    @property
+    def image_height(self):
+        return self._image_height if "_image_height" in dir(self) and self._image_height is not None\
+          else self._naxis1
+                    
     @property
     def image_offset(self):
         if "_image_offset" not in dir(self) or self._image_offset is None:
-            self.set_offset(0,0)
+            self.set_offset(0,0,None,None)
         return self._image_offset
+
+    # ----------------
+    # - Contours
+    # ----------------
+    @property
+    def contours(self,exp_order=5):
+        # -- Load it the fist time you use it
+        if "_contour" not in dir(self) or\
+           ("_reload_contours" in dir(self) and self._reload_contours):
+            self._reload_contours = False
+            from ..utils import shape
+            if not shape.HAS_SHAPELY:
+                self._contour = None
+            else:
+                a2,a1 = self.image_height,self.image_width
+                # This could be improved
+                npoints = 2+exp_order
+                # -- This defines the contours
+                width = np.linspace(0,a1,npoints)
+                height = np.linspace(0,a2,npoints)
+                v1 = np.asarray([np.ones(npoints-1)*0, width[:-1]]).T
+                v2 = np.asarray([height[:-1], np.ones(npoints-1)*a1]).T
+                v3 = np.asarray([np.ones(npoints-1)*a2, width[::-1][:-1]]).T
+                v4 = np.asarray([height[::-1][:-1], np.ones(npoints-1)*0]).T
+                
+                ra,dec = np.asarray([self.pix2world(i,j)
+                                for i,j in np.concatenate([v1,v2,v3,v4],axis=0)]).T
+                
+                self._contour = shape.get_contour_polygon(ra,dec)
+            
+        return self._contour
+
+    @property
+    def contours_pxl(self,**kwargs):
+        """Based on the contours (in wcs) and wcs2pxl, this draws the pixels contours"""
+        x,y = np.asarray([self.world2pix(ra_,dec_) for ra_,dec_ in
+                          np.asarray(self.contours.exterior.xy).T]).T # switch ra and dec ;  checked
+        return shape.get_contour_polygon(x,y)
+    
+    def has_contours(self):
+        return self.contours is not None
+
     
 class WCS(pyWCS,_MotherWCS_):
     """
@@ -102,8 +189,8 @@ class WCS(pyWCS,_MotherWCS_):
             return x+self.image_offset[1],y+self.image_offset[0]
 
         x,y = np.asarray(self.wcs_world2pix(np.asarray([ra,dec]).T.tolist(),0)).T
-        return np.asarray(x)+self.image_offset[1],\
-          np.asarray(y)+self.image_offset[0]
+        return np.asarray([np.asarray(x)+self.image_offset[1],
+                           np.asarray(y)+self.image_offset[0]]).T
 
     def coordsAreInImage(self,ra,dec):
         """
@@ -113,7 +200,8 @@ class WCS(pyWCS,_MotherWCS_):
     
     @property
     def central_coords(self):
-        return self.wcs_pix2world([[self._naxis1/2.,self._naxis2/2.]],1)[0]
+        return self.wcs_pix2world([[self.image_height/2.,
+                                    self.image_width/2.]],1)[0]
 
     @property
     def edge_size(self):
@@ -139,43 +227,6 @@ class WCS(pyWCS,_MotherWCS_):
         return  pxl[0] * units.Unit(self.wcs.cunit[0]).in_units("degree")* units.degree,\
           pxl[1] * units.Unit(self.wcs.cunit[1]).in_units("degree")* units.degree
           
-    
-    @property
-    def contours(self,exp_order=5):
-        # -- Load it the fist time you use it
-        if "_contour" not in dir(self):
-            from ..utils import shape
-            if not shape.HAS_SHAPELY:
-                self._contour = None
-            else:
-                a2,a1 = self._naxis1,self._naxis2
-                # This could be improved
-                npoints = 2+exp_order
-                # -- This defines the contours
-                width = np.linspace(0,a1,npoints)
-                heigh = np.linspace(0,a2,npoints)
-                v1 = np.asarray([np.ones(npoints-1)*0, width[:-1]]).T
-                v2 = np.asarray([heigh[:-1], np.ones(npoints-1)*a1]).T
-                v3 = np.asarray([np.ones(npoints-1)*a2, width[::-1][:-1]]).T
-                v4 = np.asarray([heigh[::-1][:-1], np.ones(npoints-1)*0]).T
-
-                ra,dec = self.pix2world(*np.concatenate([v1,v2,v3,v4],axis=0).T).T
-                
-                self._contour = shape.get_contour_polygon(ra,dec)
-            
-        return self._contour
-
-    @property
-    def contours_pxl(self,**kwargs):
-        """Based on the contours (in wcs) and wcs2pxl, this draws the pixels contours"""
-        x,y = np.asarray([self.world2pix(ra_,dec_) for ra_,dec_ in
-                          np.asarray(self.contours.exterior.xy).T]).T # switch ra and dec ;  checked
-        return shape.get_contour_polygon(x,y)
-    
-    def has_contours(self):
-        """Test if this have contours defined"""
-        return self.contours is not None
-
 
 class _WCSbackup(astWCS.WCS,_MotherWCS_):
 
@@ -205,7 +256,7 @@ class _WCSbackup(astWCS.WCS,_MotherWCS_):
         
     @property
     def central_coords(self):
-        return self.pix2world(self._naxis1/2.,self._naxis2/2.)
+        return self.pix2world(self.image_height/2.,self.image_width/2.)
 
     @property
     def edge_size(self):
@@ -223,7 +274,7 @@ class _WCSbackup(astWCS.WCS,_MotherWCS_):
     # = 
     # ===================== #
     def calc_footprint(self,center=True):
-        naxis1,naxis2 = self._naxis1,self._naxis2
+        naxis1,naxis2 = self.image_height,self.image_width
         if center == True:
             corners = np.array([[1, 1],
                                 [1, naxis2],
@@ -254,38 +305,4 @@ class _WCSbackup(astWCS.WCS,_MotherWCS_):
     # = 
     # ===================== #
     
-    @property
-    def contours(self,exp_order=5):
-        # -- Load it the fist time you use it
-        if "_contour" not in dir(self):
-            from ..utils import shape
-            if not shape.HAS_SHAPELY:
-                self._contour = None
-            else:
-                a2,a1 = self._naxis1,self._naxis2
-                # This could be improved
-                npoints = 2+exp_order
-                # -- This defines the contours
-                width = np.linspace(0,a1,npoints)
-                heigh = np.linspace(0,a2,npoints)
-                v1 = np.asarray([np.ones(npoints-1)*0, width[:-1]]).T
-                v2 = np.asarray([heigh[:-1], np.ones(npoints-1)*a1]).T
-                v3 = np.asarray([np.ones(npoints-1)*a2, width[::-1][:-1]]).T
-                v4 = np.asarray([heigh[::-1][:-1], np.ones(npoints-1)*0]).T
-                
-                ra,dec = np.asarray([self.pix2world(i,j)
-                                for i,j in np.concatenate([v1,v2,v3,v4],axis=0)]).T
-                
-                self._contour = shape.get_contour_polygon(ra,dec)
-            
-        return self._contour
-
-    @property
-    def contours_pxl(self,**kwargs):
-        """Based on the contours (in wcs) and wcs2pxl, this draws the pixels contours"""
-        x,y = np.asarray([self.world2pix(ra_,dec_) for ra_,dec_ in
-                          np.asarray(self.contours.exterior.xy).T]).T # switch ra and dec ;  checked
-        return shape.get_contour_polygon(x,y)
     
-    def has_contours(self):
-        return self.contours is not None

astrobject/collection.py
@@ -6,7 +6,7 @@
 import numpy as np
 import warnings
 from astropy.table import Table
-
+from astropy.io import fits as pf
 
 from .photometry import photopoint
 from .baseobject import BaseObject
@@ -280,18 +280,33 @@ class ImageCollection( Collection ):
     # ========================== #
     # = Get                    = #
     # ========================== #
-    def get_image(self,id,load_catalogue=True):
+    def get_image(self,id,load_catalogue=True,
+                  dataslice0=[0,-1],dataslice1=[0,-1],
+                  **kwargs):
         """
+
+        **kwargs goes to instrument's init if the image is loaded for the first time
+        and to reload_data() if not and dataslicing differ from what have been loaded before.
+        
         """
         self._test_id_(id)
         # --------------------
         # - image already exist
         if self.images[id]["image"] is None:
-            self._load_image_(id)
-            return self.get_image(id)
-        
+            self._load_image_(id,dataslice0=dataslice0,
+                                 dataslice1=dataslice1,
+                              **kwargs)
+            return self.get_image(id,dataslice0=dataslice0,
+                                    dataslice1=dataslice1)
+
         im = self.images[id]["image"]
-        
+        # ------------------
+        # - Check if slicing ok
+        if dataslice0 != im._build_properties["dataslice0"] or \
+          dataslice1 != im._build_properties["dataslice1"]:
+            # -- Remark: This update the self.images[id]["image"]
+            im.reload_data(dataslice0, dataslice1,**kwargs)
+            
         # ---------------
         # Target
         if not im.has_target() and self.has_target():
@@ -329,6 +344,40 @@ class ImageCollection( Collection ):
                                                isolated_only=isolated_only,
                                                stars_only=stars_only,
                                                catmag_range=catmag_range)
+
+    def get_pixels_slicing(self,central_coords, radius, runits="arcmin", id_=None,
+                           wcs_coords=True, clean=True):
+        """
+        The dataslice0,dataslice1 for the given 'central_coords' with a +/- 'radius' 'runits' value.
+        This slicing will be cleaned (pixels within the image FoV) except if clean is False
+        """
+        # ---------------
+        # - one id_
+        if id_ is not None:
+            self._test_id_(id_)
+            radec = np.asarray(central_coords) if not wcs_coords else\
+              np.asarray(self.images[id_]["wcs"].world2pix(*central_coords))
+            pixels   = radius*self.images[id_]["wcs"].units_to_pixels(runits).value
+            dataslice0,dataslice1 = [[radec_-pixels,radec_+pixels] for radec_ in radec[::-1]]
+            if clean:
+                # -- cleaning
+                max0 = self.images[id_]["wcs"]._naxis1
+                max1 = self.images[id_]["wcs"]._naxis2
+                if dataslice0[0] <0    : dataslice0[0] = 0
+                if dataslice1[0] <0    : dataslice1[0] = 0
+                if dataslice0[1] >max0 : dataslice0[1] = max0
+                if dataslice1[1] >max1 : dataslice1[1] = max1
+            return {"dataslice0":np.asarray(dataslice0,dtype=int).tolist(),
+                    "dataslice1":np.asarray(dataslice1,dtype=int).tolist()}
+        
+        # ---------------
+        # - get all id_
+        if id_ is None:
+            out = {}
+            for id_ in self.list_id:
+                out[id_] = self.get_pixels_slicing(central_coords, radius, runits="arcmin", id_=id_)
+            return out
+        
     # --------------------- #
     # - Get Extraction    - #
     # --------------------- #
@@ -338,8 +387,8 @@ class ImageCollection( Collection ):
         """
         idused = self.list_id if ids is None else\
           [ids] if "__iter__" not in dir(ids) else ids
+            
           
-        print "ToBeDone"
     # --------------------- #
     # - Get Target        - #
     # --------------------- #
@@ -604,10 +653,10 @@ class ImageCollection( Collection ):
             }
         return ID
         
-    def _load_image_(self,id):
+    def _load_image_(self,id,**kwargs):
         """
         """
-        self.images[id]["image"] = inst.instrument(self.images[id]["file"])
+        self.images[id]["image"] = inst.instrument(self.images[id]["file"],**kwargs)
         
     # ========================== #
     # = Properties             = #

astrobject/instruments/baseinstrument.py
@@ -366,7 +366,7 @@ class Catalogue( BaseObject ):
         elif fovcontours is None:
             raise ValueError("Either wcs or fovcontours must be provided")
         
-        self.fovmask = self.get_contour_mask(fovcontours)
+        self.fovmask = self.get_contour_mask(fovcontours, infov=False)
         self._side_properties["fovcontours"] = fovcontours
         
     def set_matchedmask(self,matchedmask):
@@ -901,7 +901,7 @@ class Catalogue( BaseObject ):
     @property
     def wcs_xy(self):
         if self.has_wcs():
-            return np.asarray(self.wcs.world2pix(self.ra,self.dec))
+            return np.asarray(self.wcs.world2pix(self.ra,self.dec)).T
         raise AttributeError("no 'wcs' solution loaded")
 
     # ----------------------
@@ -960,7 +960,25 @@ class Catalogue( BaseObject ):
     @property
     def contours(self):
         return self._derived_properties["contours"]
+
+    @property
+    def contours_pxl(self,**kwargs):
+        """Based on the contours (in wcs) and wcs2pxl, this draws the pixels contours"""
+        if not self.has_wcs():
+            raise AttributeError("no wcs solution loaded. You need one.")
+        x,y = np.asarray([self.wcs.world2pix(ra_,dec_) for ra_,dec_ in
+                          np.asarray(self.contours.exterior.xy).T]).T # switch ra and dec ;  checked
+        return shape.get_contour_polygon(x,y)
     
     @property
     def fovcontours(self):
         return self._side_properties["fovcontours"]
+
+    @property
+    def fovcontours_pxl(self,**kwargs):
+        """Based on the contours (in wcs) and wcs2pxl, this draws the pixels contours"""
+        if not self.has_wcs():
+            raise AttributeError("no wcs solution loaded. You need one.")
+        x,y = np.asarray([self.wcs.world2pix(ra_,dec_) for ra_,dec_ in
+                          np.asarray(self.fovcontours.exterior.xy).T]).T # switch ra and dec ;  checked
+        return shape.get_contour_polygon(x,y)

astrobject/photometry.py
@@ -414,8 +414,10 @@ class Image( BaseObject ):
         self._build_properties["dataslice0"] = dataslice0
         self._build_properties["dataslice1"] = dataslice1
         if self.has_wcs():
-            self.wcs.set_offset(*self._dataoffset)
-            
+            self.wcs.set_offset(*self._dataslicing)
+            if self.has_catalogue():
+                self.catalogue.set_fovmask(wcs=self.wcs)
+        
         if reload_sep: self.sep_extract()
         
     # ------------------- #
@@ -465,7 +467,7 @@ class Image( BaseObject ):
         if "__nature__" not in dir(wcs) or wcs.__nature__ != "WCS":
             raise TypeError("The given wcs solution is not a astrobject WCS instance")
         self._side_properties["wcs"] = astrometry.get_wcs(wcs)
-        self.wcs.set_offset(*self._dataoffset)
+        self.wcs.set_offset(*self._dataslicing)
         
     def set_catalogue(self,catalogue,force_it=False):
         """
@@ -826,32 +828,13 @@ class Image( BaseObject ):
 
     # - Conversion tools
     def units_to_pixels(self,units_):
-        """units should be a parsable string or a astropy units"""
-        
-        if type(units_) == str:
-            # - astropy units
+        """units should be a parsable string or a astropy units. Uses the wcs method units_to_pixels()
+        In addition, you have access to the 'psf [==fwhm]' unit"""
+        if type(units_) == str and units_.lower() in ["fwhm","psf"]:
+            units_ = self.fwhm
             
-            if units_.lower() in ["pixel","pixels","pxl","pix","pxls","pixs"]:
-                return units.Quantity(1.)
-            elif units_.lower() in ["fwhm","psf"]:
-                units_ = self.fwhm
-            elif units_.lower() in ["kpc"]:
-                if not self.has_target():
-                    raise AttributeError("You need a target to convert kpc->arcsec")
-                units_ = self.target.arcsec_per_kpc * units.arcsec
-            elif units_ in dir(units):
-                units_ = 1*units.Unit(units_)
-            else:
-                TypeError("unparsable units %s: astropy.units or pixels/pxl or kpc could be provided."%units_)
-        # ----------------
-        # - astropy Units
-        if type(units_) is units.core.Unit:
-            units_ = 1*units_
-
-        if type(units_) is units.quantity.Quantity:
-            return units_.to("arcsec") / self.pixel_size_arcsec
-        
-        raise TypeError("unparsable units: astropy.units or pixels/pxl or kpc could be provided.")
+        return self.wcs.units_to_pixels(units_,target=self.target)
+    
             
     def pixel_to_coords(self,pixel_x,pixel_y):
         """get the coordinate (ra,dec; degree) associated to the given pixel (x,y)"""
@@ -1349,8 +1332,9 @@ class Image( BaseObject ):
         self._properties['rawdata'] = np.asarray(value)
         self._update_data_()
 
+        
     @property
-    def _dataoffset(self):
+    def _dataslicing(self):
         if self._build_properties["dataslice0"] == "undefined":
             warnings.warn("No _build_properties['dataslice0'] defined. 0 assumed")
             offset0 = [0,-1]
@@ -1361,8 +1345,14 @@ class Image( BaseObject ):
             offset1 = [0,-1]
         else:
             offset1=self._build_properties["dataslice1"]
-            
-        return offset0[0],offset1[0]
+
+        if offset0[1] <0: offset0[1] = self.height+offset0[1]+2
+        if offset1[1] <0: offset1[1] = self.width +offset1[1]+2
+        return offset0[0],offset1[0],offset0[1]-offset0[0],offset1[1]-offset1[0]
+    
+    @property
+    def _dataoffset(self):
+        return self._dataslicing[:2]
             
     # Background
     @property
@@ -1454,7 +1444,6 @@ class Image( BaseObject ):
             return [ps.to("arcsec") for ps in self.pixel_size_deg]
         return self.pixel_size_deg.to("arcsec")
 
-    
     # ----------------      
     # -- target tools
     @property
@@ -1476,7 +1465,7 @@ class Image( BaseObject ):
           else False
           
     @property
-    def sepmask(self,r=5):
+    def sepmask(self,r=10):
         if not self.has_sepobjects():
             raise AttributeError("No sepobjects loaded. Run sep_extract")
         return self.sepobjects.get_ellipse_mask(self.width,self.height,r=r)