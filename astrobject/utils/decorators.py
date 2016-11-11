d48303dc9f97100802065873d7baa0251f760fe3
Mickael <mick.rigault@gmail.com>
2016-11-11 17:39:31 +0100

    New fancy decorator

--- astrobject/utils/decorators.py
+++ astrobject/utils/decorators.py
@@ -36,3 +36,14 @@ def make_method(obj):
         return f
 
     return decorate
+
+
+def speed_test(func):
+    def wrapper(*args, **kwargs):
+        t1 = time.time()
+        for x in xrange(5000):
+            results = func(*args, **kwargs)
+        t2 = time.time()
+        print '%s took %0.3f ms' % (func.func_name, (t2-t1)*1000.0)
+        return results
+    return wrapper
