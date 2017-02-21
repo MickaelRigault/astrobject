dfc5e2ba6194a7cd1b1e2685bd48ec6cdb17bee7
Mickael <mick.rigault@gmail.com>
2017-02-21 20:22:01 +0100

    PanStarrs improved

---
+++ astrobject/instruments/panstarrs.py
@@ -0,0 +1,187 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import warnings
+import numpy as np
+
+# - astropy
+from astropy.io      import fits as pf
+from astropy         import time
+
+# - local dependencies
+from .baseinstrument    import Instrument
+from ..photometry       import get_photopoint
+from ..utils.decorators import _autogen_docstring_inheritance
+from ..utils.tools      import kwargs_update
+
+PANSTARRS_INFO= {"bands":["ps1.g","ps1.r","ps1.i","ps1.z","ps1.z"],
+                 "telescope":{
+                     "lon": None,
+                     "lat": None}}
+"""
+Info from Magnier 2016
+
+Seeing:
+-------
+the median image quality for the 3π survey is FWHM = (1.31, 1.19, 1.11, 1.07, 1.02) 
+arcseconds for (gP1,rP1,iP1,zP1,yP1), with a floor of ∼ 0.7 arcseconds
+
+
+First, 3 PSF-convolved galaxy models (Sersic, DeVaucouleurs, Exponential)
+"""
+                 
+DATAINDEX = 0
+
+
+# -------------------- #
+# - Instrument Info  - #
+# -------------------- #
+    
+def panstarrs(warps_image, weight_image=None, **kwargs):
+    """ """
+    return PanSTARRS(warps_image,weightfilename=weight_image,
+                        **kwargs)
+
+def is_panstarrs_file(filename):
+    """This test if the input file is a GALEX one. Test if 'MPSTYPE' is in the header """
+    # not great but this is the structure of MJC images
+    return "PSCAMERA" in pf.getheader(filename).keys()
+
+def which_band_is_file(filename):
+    """This resuts the band of the given file if it is a
+    sdss one"""
+    if not is_panstarrs_file(filename):
+        return None
+        
+    h_ = pf.getheader(filename)
+    return h_.header.get("HIERARCH FPA.FILTER",None).split(".")[0]
+    
+def which_obs_mjd(filename):
+    """ read the galex-filename and return the
+    modified julian date """
+    if not is_panstarrs_file(filename):
+        return None
+    h_ = pf.getheader(filename)
+    return h_.get("MJD-OBS")
+
+
+
+class PanSTARRS( Instrument ):
+    """ """
+    PROPERTIES = ["weightmap"]
+    instrument_name = "PanSTARRS"
+    
+    def __init__(self,filename=None, weightfilename=None,
+                maskfilename=None, background=None, astrotarget=None,
+                data_index=0, dataslice0=None, dataslice1=None,
+                 empty=False, **kwargs):
+        """
+        Initalize the image by giving its filelocation (*filename*). This
+        will load it using the load() method.
+
+        Parameters
+        ----------
+        filename: [string.fits]    
+            fits file from where the image will be loaded
+            Set None and no image will be loaded
+
+        weightfilename: [string.fits]
+            fits file containing the weight map.
+
+        astrotarget: [AstroTarget] An AstroTarget object you which to associate
+                                   to this image. 
+                                   
+        empty: [bool]              Does not do anything, just loads an empty object.
+                                   (Careful with that)
+                                   
+        """
+        self.__build__(data_index=data_index)
+
+        if empty:
+            return
+
+        if filename is not None:
+            force_it = kwargs.pop("force_it",True)
+            self.load(filename,force_it=force_it,
+                      dataslice0=dataslice0,
+                      dataslice1=dataslice1,
+                      background=0,
+                      **kwargs)
+
+        if weightfilename is not None:
+            self.set_weightimage(weightfilename)
+
+        if maskfilename is not None:
+            self.set_maskdata(maskmap)
+            
+        # - Set the target if any
+        if astrotarget is not None:
+            self.set_target(astrotarget)
+            if self.has_weightimage():
+                self.weightimage.set_target(astrotarget)
+
+        # - Background
+        self.set_background(background, force_it=True)
+
+
+    def __build__(self, *args, **kwargs):
+        """ """
+        super(PanSTARRS, self).__build__(*args, **kwargs)
+        self._build_properties["bkgdbox"] = {"bh":100,"bw":100,"fh":3,"fw":3}
+
+                
+    def set_weightimage(self, weightmap):
+        """ Attach the Weight Maps of the Warps images. 
+        (Weight images are variance maps see
+        https://confluence.stsci.edu/display/PANSTARRS/PS1+Weight+image)
+        """
+        self._properties["weightmap"] = PanSTARRS(weightmap, astrotarget=self.target, background=0)
+
+    def set_maskdata(self, maskmap):
+        mask = PanSTARRS(maskmap,  background=0)
+        super(PanSTARRS,self).set_maskdata(mask.rawdata)
+        
+    # ---------------------
+    # - PS structure with wt
+
+    @property
+    def weightimage(self):
+        """ The weight map. (see set_weightimage())"""
+        return self._properties["weightmap"]
+    
+    def has_weightimage(self):
+        """ Has the weight image been set? True means yes """
+        return self.weightimage is not None
+
+    @property
+    def rawdata(self):
+        """ The data in the PanStarrs images are not per second. """
+        return super(PanSTARRS,self).rawdata / self.exposuretime
+    
+    @property
+    def var(self):
+        """ The weight maps since:
+           '''Weight images are variance maps'''
+           - https://confluence.stsci.edu/display/PANSTARRS/PS1+Weight+image
+        """
+        if not self.has_weightimage():
+            return None
+        # Since rawdata have to be devided by exptime -> variance byt exptime**2
+        return self.weightimage.rawdata  / self.exposuretime 
+    
+    # ---------------------
+    # - Generic Properties
+    @property
+    def mab0(self):
+        return self.header["HIERARCH FPA.ZP"]
+    
+    @property
+    def bandname(self):
+        """ band of the image. """
+        return "ps1.%s"%self.header.get("HIERARCH FPA.FILTER",None).split(".")[0]
+
+    @property
+    def mjd(self):
+        return self.header["MJD-OBS"]
